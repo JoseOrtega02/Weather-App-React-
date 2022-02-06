8ff4c02d176b23b88c4d244f4db90276209e8da1
jose ortega <ortegajoseweb02mail.com>
2022-02-06 20:33:17 -0300

    weather app v1.1

--- src/Main.jsx
+++ src/Main.jsx
@@ -43,7 +43,31 @@ export  function Main() {
         return `${day} ${date} ${month} ${year}`
       }
     
-     
+     const expand = () => {
+         const ul = document.getElementById('ul');
+         const icon = document.getElementById('icon');
+         
+
+        if (ul.style.display === 'none') {
+            ul.style.display = 'block';
+            icon.style.animationName = "rotate";
+            icon.style.transform = "rotate(180deg)";
+            ul.style.animationName = "visible";
+            ul.style.opacity = 1;
+            
+        }else{
+            setTimeout(() => {
+                ul.style.display = 'none'
+            },800)
+            
+            icon.style.animationName = "unrotate"
+            icon.style.transform = "rotate(0deg)"
+            ul.style.animationName = "hidden";
+            ul.style.opacity = 0;
+        }
+        
+         
+     } 
 
       
 
@@ -63,7 +87,7 @@ export  function Main() {
             onKeyPress={handle}
           />
           {(typeof actualCity.main != "undefined")?(
-            <div className="Main__container">
+            <div className="Main__container" id="main">
             <img src={`https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${actualCity.weather[0].icon}.svg`} alt=""/>
              <div className="Main__container--text">
                  <h4>{dateBuilder(new Date())}</h4>
@@ -71,6 +95,18 @@ export  function Main() {
                  <h3>Temperature: {actualCity.main.temp}°C</h3>
                  <h3 className="wheather">{actualCity.weather[0].main}</h3>
                  <h4>{actualCity.weather[0].description}</h4>
+                 <div>
+                 <h4>More Details:</h4>
+                     <ul  id="ul">
+                         <li id="li">Humidity: {actualCity.main.humidity}%</li>
+                         <li >Pressure: {actualCity.main.pressure} hPa</li>
+                         <li >Tempperature max: {actualCity.main.temp_max}°C</li>
+                         <li >Temperature min: {actualCity.main.temp_min}°C</li>
+                     </ul>
+                     <div>
+                         <i onClick={expand} ><span id="icon" className="material-icons-outlined">expand_more</span></i>
+                     </div>
+                 </div>
              </div>
              
            </div>
